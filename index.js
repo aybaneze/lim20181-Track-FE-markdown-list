e1b6203f36545cfe291b7f189cd87abe289c5103
aybaneze <andrea270696@gmail.com>
2018-08-21 18:24:48 -0500

    subiendo el inicio del proyecto

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 #!/usr/bin/env node
 
-const [,, ...args] = process.argv
+//Guardamos en variable el modulo de fileSystem
+const fs = require('fs'); 
+const path = require('path');
 
-console.log(`hello world ${args}`)
+const options = {
+    validate: false,
+    stats: false
+}
+
+
+fs.readFile('Readme.md', (err, data) => {
+    if(err) {
+       data.forEach(e => {
+       console.log("ingrese al archivo")
+ });
+    } else {
+      console.log(data);
+      console.log("no se reconoce el archivo");
+    }
+  });
+
