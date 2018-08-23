0c21de5a7b018ece2958e149be63ac4f1bb6a7e5
aybaneze <andrea270696@gmail.com>
2018-08-23 02:19:22 -0500

    leyendo link

--- index.js
+++ index.js
@@ -4,20 +4,51 @@
 const fs = require('fs'); 
 const path = require('path');
 
-const options = {
-    validate: false,
-    stats: false
+// const options = {
+//     validate: false,
+//     stats: false
+// }
+
+const exp = /\(((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
+
+const ReadFiles = (path) =>{
+fs.readFile(path,'utf-8', (err, data) => {
+    if(err) {
+        throw err
+    } else {
+      ReadData(data)
+    }
+  }); 
 }
 
+ReadFiles('README.md');
 
-fs.readFile('Readme.md', (err, data) => {
-    if(err) {
-       data.forEach(e => {
-       console.log("ingrese al archivo")
- });
-    } else {
-      console.log(data);
-      console.log("no se reconoce el archivo");
-    }
-  });
+countLink = [];
 
+const ReadData = (file) => {
+  let result = file.match(exp);  
+result.forEach(element => {
+  countLink= element;
+  const expF = ")"
+  let res = parseInt(element.indexOf(expF));
+  let link = element.substring(1,res);
+  console.log(link);
+ console.log(countLink)
+ }) 
+};
+
+
+
+const validate = (element) => {
+  fetch(element)
+  .then((response)=>{
+    if(response.status < 400){
+    console.log("fail");
+  } else{
+    console.log("ok");
+  }
+  })
+  
+
+    
+  }
